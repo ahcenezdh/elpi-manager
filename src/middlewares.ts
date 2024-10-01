ed9a8b4e058a988ffac808c0a7cef4267570976e
Space V <40030799+ahcenezdh@users.noreply.github.com>
2024-10-01 11:07:24 +0200

    setup(base)

---
+++ src/middlewares.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+
+import ErrorResponse from './pages/interfaces/ErrorResponse';
+
+//********** Middlewares **********//
+export const notFound = (
+  request: Request,
+  response: Response,
+  nextFunction: NextFunction,
+) => {
+  response.status(404);
+  const error = new Error(`Not found - ${request.originalUrl}`);
+  nextFunction(error);
+};
+
+export const errorHandler = (
+  error: Error,
+  request: Request,
+  response: Response<ErrorResponse>,
+  // Next function, but not used here
+) => {
+  const statusCode = response.statusCode !== 200 ? response.statusCode : 500;
+  response
+    .status(statusCode)
+    .json({ message: error.message, stack: error.stack });
+};
